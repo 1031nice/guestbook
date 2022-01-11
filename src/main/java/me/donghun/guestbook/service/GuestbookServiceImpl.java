01d3175f60c1561c2fbf3bf7b55831a9bb93ac8b
Dong-Hun <1031nice@gmail.com>
2022-01-11 15:04:49 +0900

    feat: GuestbookService dtoToEntity 메소드 구현

--- src/main/java/me/donghun/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/me/donghun/guestbook/service/GuestbookServiceImpl.java
@@ -1,2 +1,12 @@
-package me.donghun.guestbook.service;public class GuestbookServiceImpl {
+package me.donghun.guestbook.service;
+
+import me.donghun.guestbook.dto.GuestbookDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GuestbookServiceImpl implements GuestbookService {
+    @Override
+    public Long register(GuestbookDTO dto) {
+        return null;
+    }
 }
