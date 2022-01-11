35e366d6e32b5be74bc07f8ed384bf3dcbb6fd48
Dong-Hun <1031nice@gmail.com>
2022-01-11 15:09:07 +0900

    feat: GuestbookService register 메소드 테스트

--- src/test/java/me/donghun/guestbook/service/GuestbookServiceTest.java
+++ src/test/java/me/donghun/guestbook/service/GuestbookServiceTest.java
@@ -2,14 +2,39 @@ package me.donghun.guestbook.service;
 
 import me.donghun.guestbook.dto.GuestbookDTO;
 import me.donghun.guestbook.entity.Guestbook;
+import me.donghun.guestbook.repository.GuestbookRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 class GuestbookServiceTest {
 
-    private final GuestbookService guestbookService = new GuestbookServiceImpl();
+    @Autowired
+    private GuestbookService guestbookService;
+
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    @DisplayName("등록")
+    @Transactional
+    void register() {
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .gno(999L)
+                .title("title")
+                .content("content")
+                .writer("writer")
+                .build();
+
+        guestbookService.register(guestbookDTO);
+
+        assertThat(guestbookRepository.findById(guestbookDTO.getGno())).isPresent();
+    }
 
     @Test
     @DisplayName("DTO를 엔티티 클래스로 변환")
