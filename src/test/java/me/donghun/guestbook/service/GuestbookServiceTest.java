129aa3136fc112feee7248c1958e107d31a70c27
Dong-Hun <1031nice@gmail.com>
2022-01-11 15:04:14 +0900

    feat: GuestbookService dtoToEntity 메소드 테스트

--- src/test/java/me/donghun/guestbook/service/GuestbookServiceTest.java
+++ src/test/java/me/donghun/guestbook/service/GuestbookServiceTest.java
@@ -1,4 +1,40 @@
-import static org.junit.jupiter.api.Assertions.*;
+package me.donghun.guestbook.service;
+
+import me.donghun.guestbook.dto.GuestbookDTO;
+import me.donghun.guestbook.entity.Guestbook;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class GuestbookServiceTest {
-  
+
+    private final GuestbookService guestbookService = new GuestbookServiceImpl();
+
+    @Test
+    @DisplayName("DTO를 엔티티 클래스로 변환")
+    void dtoToEntity() {
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .gno(1L)
+                .title("title")
+                .content("content")
+                .writer("writer")
+                .build();
+
+        Guestbook guestbook = Guestbook.builder()
+                .gno(1L)
+                .title("title")
+                .content("content")
+                .writer("writer")
+                .build();
+
+        Guestbook afterConvert = guestbookService.dtoToEntity(guestbookDTO);
+        assertThat(afterConvert.getGno()).isEqualTo(guestbook.getGno());
+        assertThat(afterConvert.getTitle()).isEqualTo(guestbook.getTitle());
+        assertThat(afterConvert.getContent()).isEqualTo(guestbook.getContent());
+        assertThat(afterConvert.getWriter()).isEqualTo(guestbook.getWriter());
+        assertThat(afterConvert.getModDate()).isNull();
+        assertThat(afterConvert.getRegDate()).isNull();
+    }
+
 }
