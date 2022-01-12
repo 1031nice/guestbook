3a7450ab749b091e4bc92c4a21eb99bc0fcaf2e8
Dong-Hun <1031nice@gmail.com>
2022-01-12 10:44:32 +0900

    feat: 등록 API 테스트

--- src/test/java/me/donghun/guestbook/controller/GuestbookControllerTest.java
+++ src/test/java/me/donghun/guestbook/controller/GuestbookControllerTest.java
@@ -1,13 +1,17 @@
 package me.donghun.guestbook.controller;
 
+import me.donghun.guestbook.repository.GuestbookRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,9 +22,29 @@ class GuestbookControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    GuestbookRepository guestbookRepository;
+
+    @Test
+    @DisplayName("등록")
+    @Transactional
+    void register() throws Exception {
+        String title = "test_title";
+        mockMvc.perform(post("/guestbook/register")
+                        .param("title", title)
+                        .param("content", "test_content")
+                        .param("writer", "test_writer"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/guestbook/list"))
+                .andExpect(flash().attributeExists("msg"))
+                .andDo(print());
+
+        assertThat(guestbookRepository.findByTitle(title)).isNotEmpty();
+    }
+
     @Test
     @DisplayName("등록 화면")
-    void register() throws Exception {
+    void registerView() throws Exception {
         mockMvc.perform(get("/guestbook/register"))
                 .andExpect(view().name("guestbook/register"))
                 .andExpect(status().isOk())
@@ -37,7 +61,7 @@ class GuestbookControllerTest {
 
     @Test
     @DisplayName("목록 조회")
-    void list() throws Exception {
+    void listView() throws Exception {
         mockMvc.perform(get("/guestbook/list"))
                 .andExpect(view().name("guestbook/list"))
                 .andExpect(model().attributeExists("result"))
